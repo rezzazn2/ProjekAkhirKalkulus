0a96d5fe3d0dc05bc93d664f4a40e40fecc18c2a
rezzazn2 <muhammadreza1665@gmail.com>
2024-11-10 16:34:51 +0700

    progress 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-// Select the navbar element
+
 const navbar = document.querySelector('nav');
 
-// Add a scroll event listener
+
 window.addEventListener('scroll', () => {
-    // Check if scroll position is greater than 0
     if (window.scrollY > 0) {
-        // Add a class to change the navbar style
-        console.log("oke");
         navbar.classList.add('scrolled');
     } else {
-        // Remove the class when at the top
         navbar.classList.remove('scrolled');
     }
 });
